ef0e849a498a69673a31a8041360f58f6cd5cb48
Mariya <mash.dobarina@gmail.com>
2021-09-21 23:20:49 +0300

    Добавлен метод add - добавление книг

--- bookshelf.js
+++ bookshelf.js
@@ -1,5 +1,18 @@
 "use strict";
 
 let bookshelf = function () {
+    let books = [];
+    return {
+            add: function (name, author, price) {
+                let book = {
+                    name: name,
+                    author: author,
+                    price: price
+                };
 
+                books.push(book);
+            }
+    };
 }();
+
+
