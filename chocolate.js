343f51c0a1beb075e53ac735a3d65b9a07e9eacf
Mariya <mash.dobarina@gmail.com>
2021-09-26 15:27:53 +0300

    Исправлен метод add

--- chocolate.js
+++ chocolate.js
@@ -2,19 +2,24 @@
 let chocolate = function () {
     let choco = [];
     return {
-        add: function(name, type, price) {
-            let availableType = [белый, черный, молочный];
-            for(let types of availableType){
-                if (type = types) {
-                    let chocolate = {
-                        name: name, 
-                        type: type,
-                        price: price
-                    }
-                    choco.push(chocolate);
+        add: function(name, type, weight, price) {
+            let availableType = ['белый', 'черный', 'молочный'];
+            let check = availableType.includes(type);
+            if (check === true)    {
+                let item = {
+                    name: name,
+                    type: type,
+                    weight: weight,
+                    price: price
                 }
+                choco.push(item);
             }
-        }
+            else {
+                console.log('Невозможно добавить указанный тип.');
+            }
+            
+        },
+
     }
 }();
 
