4c856717e98584df71fe5530768493cbe6984145
Mariya <mash.dobarina@gmail.com>
2021-09-26 14:26:39 +0300

    Добавлен метод add - добавление шоколадки

--- chocolate.js
+++ chocolate.js
@@ -1,4 +1,21 @@
 "use strict";
 let chocolate = function () {
-
+    let choco = [];
+    return {
+        add: function(name, type, price) {
+            let availableType = [белый, черный, молочный];
+            for(let types of availableType){
+                if (type = types) {
+                    let chocolate = {
+                        name: name, 
+                        type: type,
+                        price: price
+                    }
+                    choco.push(chocolate);
+                }
+            }
+        }
+    }
 }();
+
+
